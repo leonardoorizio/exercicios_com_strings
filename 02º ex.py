858627117c3d06802b1577372f5bda74cd14caba
Leonardo Orizio <102883640+leonardoorizio@users.noreply.github.com>
2022-10-08 09:21:46 -0300

    Add files via upload

---
+++ 02º ex.py
@@ -0,0 +1,8 @@
+'''
+Nome ao contrário em maiúsculas. Faça um programa que permita ao usuário digitar o seu nome e em seguida mostre o nome do usuário de trás para frente utilizando
+somente letras maiúsculas. Dica: lembre−se que ao informar o nome o usuário pode digitar letras maiúsculas ou minúsculas.
+'''
+
+nome = str.upper(input('Digite seu nome:'))
+
+print(nome[::-1])
