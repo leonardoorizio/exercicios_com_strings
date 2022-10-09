b083a6df2a26287074dbf906c6bf82b981f8a8a7
Leonardo Orizio <102883640+leonardoorizio@users.noreply.github.com>
2022-10-09 15:35:58 -0300

    Add files via upload

---
+++ 03º ex.py
@@ -0,0 +1,15 @@
+'''
+Nome na vertical. Faça um programa que solicite o nome do usuário e imprima-o na vertical.
+
+F
+U
+L
+A
+N
+O
+'''
+
+nome = input('Digite seu nome:')
+
+for i in nome:
+    print(i)
