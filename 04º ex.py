c8187f32fe20311bf3a7e2899cc9b6e21d019e2b
Leonardo Orizio <102883640+leonardoorizio@users.noreply.github.com>
2022-10-12 06:06:36 -0300

    Add files via upload

---
+++ 04º ex.py
@@ -0,0 +1,15 @@
+'''
+Nome na vertical em escada invertida. Altere o programa anterior de modo que a escada seja invertida.
+
+FULANO
+FULAN
+FULA
+FUL
+FU
+F
+'''
+
+s = 'FULANO'
+while s != '':
+	print(s)
+	s = s[:-1]
